a95c3bf615a49c113489f12689e59bd8e6c4d5be
davidbarnabas <35915198+davidbarnabas@users.noreply.github.com>
2018-02-26 13:28:43 +0530

    Create kk.c

---
+++ kk.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+char str[20];
+int i;
+printf("Enter the string:");
+
+scanf("%s",&str);
+
+for(i=0;str[i]!='\0';i++)
+
+{
+
+if(str[i]=='0'||str[i]=='1')
+printf("YES");
+}
+
+
+}
